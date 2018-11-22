11a353e92b9d34b37d6e8a4050aef63be6529f7d
christphrd <diep.christopher@gmail.com>
2018-11-22 00:09:33 -0500

    user controller create action coding

--- backend-stock-portfolio/app/controllers/users_controller.rb
+++ backend-stock-portfolio/app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
     def create
+        @user = User.new(user_params)
         byebug
     end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
