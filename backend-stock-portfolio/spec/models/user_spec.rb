7dca322ffcf81fb354891e0a71dc22e960b6931f
christphrd <diep.christopher@gmail.com>
2019-02-26 20:39:56 -0500

    added username presence test

--- backend-stock-portfolio/spec/models/user_spec.rb
+++ backend-stock-portfolio/spec/models/user_spec.rb
@@ -1,30 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  User.destroy_all
+  context "validation tests" do
+    User.destroy_all
+
+    it "ensures username presence" do
+      user = User.new(email: "test@test.com", password: "test", password_confirmation: "test").save
+      expect(user).to eq(false)
+    end
+  end
   
-  user = User.create(
-    name: "test",
-    email: "test@test.com",
-    password: "test",
-    password_confirmation: "test"
-  )
+  context "user story tests" do
+    User.destroy_all
 
-  it "can be created with name, email, and password" do
-    expect(user).to be_valid
-  end
-
-  it "has a default account balance of $5000" do
-    expect(user.account_balance).to eq(5000)
-  end
-
-  it "can only be registered once with a given email" do
-    user2 = User.new(
+    user = User.create(
       name: "test",
       email: "test@test.com",
       password: "test",
       password_confirmation: "test"
     )
-    expect(user2).to be_invalid
+    
+    it "can be created with name, email, and password" do
+      expect(user).to be_valid
+    end
+
+    it "has a default account balance of $5000" do
+      expect(user.account_balance).to eq(5000)
+    end
+
+    it "can only be registered once with a given email" do
+      user2 = User.new(
+        name: "test",
+        email: "test@test.com",
+        password: "test",
+        password_confirmation: "test"
+      )
+      expect(user2).to be_invalid
+    end
   end
 end
