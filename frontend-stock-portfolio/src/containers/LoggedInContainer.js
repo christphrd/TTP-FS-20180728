4af372ea0bbd53c6a443fff0e16971477cd35a1d
christphrd <diep.christopher@gmail.com>
2019-04-26 09:18:30 -0400

    Loggedincontainer functional. hook

--- frontend-stock-portfolio/src/containers/LoggedInContainer.js
+++ frontend-stock-portfolio/src/containers/LoggedInContainer.js
@@ -7,39 +7,36 @@ import Button from '@material-ui/core/Button';
 
 import { backendBaseURL, authorizedHeaders} from '../constants';
 
-class LoggedInContainer extends React.Component {
-    state = {
-        onAudit: false,
-        transactions: []
-    };
+const LoggedInContainer = props => {
+    const [onAudit, setOnAudit] = React.useState(false);
+    const [transactions, setTransactions] = React.useState([]);
 
-    handleNavClick = e => {
+    const handleNavClick = e => {
         const settings = { headers: authorizedHeaders() };
         
         fetch(`${backendBaseURL}audit`, settings)
         .then(res => res.json())
-        .then(json => this.setState({
-            transactions: json,
-            onAudit: !this.state.onAudit
-        }))
-        .catch(err => this.setState({
-            onAudit: !this.state.onAudit
-        }, () => console.log(err)))
+        .then(json => {
+            setOnAudit(!onAudit)
+            setTransactions(json)
+        })
+        .catch(err => {
+            setOnAudit(!onAudit)
+            console.log(err)
+        })
     };
     
-    render() {
-        const {account_balance, email, name} = this.props.userData;
-        return (
-            <div>
-                <Button variant="contained" color="secondary" onClick={this.props.logOut}>Log Out</Button>
-                <Button color="primary" onClick={this.handleNavClick}>{this.state.onAudit ? "Go to Portfolio" : "Go to Transactions"}</Button>
-                <h2>
-                    Hi {name}, this is your email: {email}.
-                </h2>
-                {this.state.onAudit ? <Audit transactions={this.state.transactions} /> : <div id="portfolio-container"><Portfolio /> <BuyTransaction balance={Number(account_balance).toFixed(2)} buyShares={this.props.buyShares}/></div>}
-            </div>
-        )
-    }
+    const {account_balance, email, name} = props.userData;
+    return (
+        <div>
+            <Button variant="contained" color="secondary" onClick={props.logOut}>Log Out</Button>
+            <Button color="primary" onClick={handleNavClick}>{onAudit ? "Go to Portfolio" : "Go to Transactions"}</Button>
+            <h2>
+                Hi {name}, this is your email: {email}.
+            </h2>
+            {onAudit ? <Audit transactions={transactions} /> : <div id="portfolio-container"><Portfolio /> <BuyTransaction balance={Number(account_balance).toFixed(2)} buyShares={props.buyShares}/></div>}
+        </div>
+    )
 };
 
 export default LoggedInContainer;
