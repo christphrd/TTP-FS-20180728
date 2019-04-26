47c1118ca0f4c12fa0d7736a12fccbe0ed541bfa
christphrd <diep.christopher@gmail.com>
2019-04-26 06:51:46 -0400

    home component now functional

--- frontend-stock-portfolio/src/containers/Home.js
+++ frontend-stock-portfolio/src/containers/Home.js
@@ -8,31 +8,36 @@ import CardContent from '@material-ui/core/CardContent';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 
-class Home extends React.Component {
-    state = {
-        onRegister: false
-    };
+// class Home extends React.Component {
+//     state = {
+//         onRegister: false
+//     };
 
-    handleNavClick = e => {
-        this.setState({
-            onRegister: !this.state.onRegister
-        })
-    };
+//     handleNavClick = e => {
+//         this.setState({
+//             onRegister: !this.state.onRegister
+//         })
+//     };
 
-    render() {
-        return(
-            <div>
-                <Card id="home-card" raised={true}>
-                    <CardContent>
-                        {this.state.onRegister ? <RegistrationForm handleRegister={this.props.handleRegister} /> : <SignInForm handleSignIn={this.props.handleSignIn} /> }
-                        <Divider />
-                        <p>OR</p>
-                        <Button variant="contained" color="default" onClick={this.handleNavClick}>{this.state.onRegister ? "Go to Sign In" : "Create an Account" }</Button>
-                    </CardContent>
-                </Card>
-            </div>
-        )
-    }
+//     render() {
+const Home = props => {
+    const [onRegister, setOnRegister] = React.useState(false);
+
+    const handleNavClick = e => setOnRegister(!onRegister);
+
+    return(
+        <div>
+            <Card id="home-card" raised={true}>
+                <CardContent>
+                    {onRegister ? <RegistrationForm handleRegister={props.handleRegister} /> : <SignInForm handleSignIn={props.handleSignIn} /> }
+                    <Divider />
+                    <p>OR</p>
+                    <Button variant="contained" color="default" onClick={handleNavClick}>{onRegister ? "Go to Sign In" : "Create an Account" }</Button>
+                </CardContent>
+            </Card>
+        </div>
+    )
+    // }
 };
 
 export default Home;
